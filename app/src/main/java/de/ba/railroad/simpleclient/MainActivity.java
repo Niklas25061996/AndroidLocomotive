c73880ecd47df2daf7d76d034e96dbe479348e78
jonas <jonas.kowalick@gmail.com>
2018-02-07 15:40:44 +0100

    reworked UI, UI reacts to changes

--- app/src/main/java/de/ba/railroad/simpleclient/MainActivity.java
+++ app/src/main/java/de/ba/railroad/simpleclient/MainActivity.java
@@ -1,12 +1,20 @@
 package de.ba.railroad.simpleclient;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.VideoView;
+import android.widget.Button;
+
+import android.net.Uri;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -44,6 +52,40 @@ public class MainActivity extends AppCompatActivity implements LocomotiveJSONPro
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView) findViewById(R.id.stream);
+        Uri uri = Uri.parse("http://www.tt-modellbahn-weimar.de/show/videos/beladung.mp4");
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.speedSlider);
+
+        SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+
+            int speed = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                speed = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // called when the user first touches the SeekBar
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                locomotive.setSpeed(speed);
+                // called after the user finishes moving the SeekBar
+            }
+        };
+
+        seekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+
+        // change the train once so the ui gets updated
+        //locomotive.setDrivingSound(!locomotive.isDrivingSound());
+        //locomotive.setDrivingSound(!locomotive.isDrivingSound());
 
         // listener to display errors
         Response.ErrorListener locomotiveErrorListener = error -> {
@@ -86,23 +128,12 @@ public class MainActivity extends AppCompatActivity implements LocomotiveJSONPro
 
         findViewById(R.id.backward).setOnClickListener(v -> locomotive.setDirection(Locomotive.DIRECTION_BACKWARD));
 
-        findViewById(R.id.stop).setOnClickListener(v -> locomotive.setSpeed(0));
 
-        SeekBar seekBar = findViewById(R.id.speedSlider);
-        seekBar.setOnSeekBarChangeListener(seekBarChangeListener);
-
-        int progress = seekBar.getProgress();
-        tvProgressLabel = findViewById(R.id.sliderText);
-        tvProgressLabel.setText("Progress: " + progress);
-
-        findViewById(R.id.send).setOnClickListener(v -> {
-            TextView speedView = (TextView) MainActivity.this.findViewById(R.id.speed);
-            int speed = Integer.parseInt(String.valueOf(speedView.getText()));
-            locomotive.setSpeed(speed);
-        });
 
         // associate light buttons
         findViewById(R.id.horn).setOnClickListener(v -> locomotive.setHornSound(!locomotive.isHornSound()));
+
+        findViewById(R.id.light).setOnClickListener(v -> locomotive.setHeadLight(!locomotive.isHeadLight()));
 
         // listener to display errors
         Response.ErrorListener switchErrorListener = error -> {
@@ -110,84 +141,7 @@ public class MainActivity extends AppCompatActivity implements LocomotiveJSONPro
             errorView.setText(error.getMessage());
         };
 
-        // Adapter for the switchSpinner view element. If we add or remove a SwitchServer
-        // here, the view will be updated and the user can select this server to control a switch group.
-        SwitchListAdapter switchListAdapter = new SwitchListAdapter(this, SWITCH_SERVER, requestQueue, switchErrorListener);
-        Spinner switchSpinner = (Spinner) findViewById(R.id.switchSpinner);
-        switchSpinner.setAdapter(switchListAdapter);
-
-        switchGroup = new SwitchJSONProxy(requestQueue, switchErrorListener, this);
-        switchGroup.setSwitchServer(null);
-
-        switchSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // get the current locomotive server
-                SwitchServer switchServer = (SwitchServer) parent.getAdapter().getItem(position);
-                switchGroup.setSwitchServer(switchServer);
-
-                TextView errorView = (TextView) MainActivity.this.findViewById(R.id.switchErrors);
-                errorView.setText(switchServer.getRestURL());
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                Log.d("main", "nothing selected");
-                switchGroup.setSwitchServer(null);
-            }
-        });
-
-        findViewById(R.id.sw1).setOnClickListener(v -> {
-            if (switchGroup.getSwitchTrack1() == SwitchGroup.TRACK_STRAIGHT) {
-                switchGroup.setSwitchTrack1(SwitchGroup.TRACK_DIVERGING);
-            } else {
-                switchGroup.setSwitchTrack1(SwitchGroup.TRACK_STRAIGHT);
-            }
-        });
-
-        findViewById(R.id.sw2).setOnClickListener(v -> {
-            if (switchGroup.getSwitchTrack2() == SwitchGroup.TRACK_STRAIGHT) {
-                switchGroup.setSwitchTrack2(SwitchGroup.TRACK_DIVERGING);
-            } else {
-                switchGroup.setSwitchTrack2(SwitchGroup.TRACK_STRAIGHT);
-            }
-        });
-
-        findViewById(R.id.sw3).setOnClickListener(v -> {
-            if (switchGroup.getSwitchTrack3() == SwitchGroup.TRACK_STRAIGHT) {
-                switchGroup.setSwitchTrack3(SwitchGroup.TRACK_DIVERGING);
-            } else {
-                switchGroup.setSwitchTrack3(SwitchGroup.TRACK_STRAIGHT);
-            }
-        });
-
-        findViewById(R.id.sw4).setOnClickListener(v -> {
-            if (switchGroup.getSwitchTrack4() == SwitchGroup.TRACK_STRAIGHT) {
-                switchGroup.setSwitchTrack4(SwitchGroup.TRACK_DIVERGING);
-            } else {
-                switchGroup.setSwitchTrack4(SwitchGroup.TRACK_STRAIGHT);
-            }
-        });
     }
-
-    SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
-
-        @Override
-        public void onProgressChanged(SeekBar seekBar, int speed, boolean fromUser) {
-            tvProgressLabel.setText("Speed " + speed);
-        }
-
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
-            // called when the user first touches the SeekBar
-        }
-
-        @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
-            locomotive.setSpeed(speed);
-            // called after the user finishes moving the SeekBar
-        }
-    };
 
     /**
      * Update the content of all GUI elements with the <code>locomotive</code>
@@ -196,9 +150,64 @@ public class MainActivity extends AppCompatActivity implements LocomotiveJSONPro
      */
     @Override
     public void locomotiveChanged(Locomotive locomotive) {
-        // visualize current speed
-        // EditText speedView = (EditText) MainActivity.this.findViewById(R.id.speed);
-        // speedView.setText("" + Math.abs(locomotive.getSpeed()));
+        TextView lightView = (TextView) findViewById(R.id.stat2);
+        TextView soundView = (TextView) findViewById(R.id.stat4);
+        TextView smokeView = (TextView) findViewById(R.id.stat6);
+        TextView trainView = (TextView) findViewById(R.id.stat8);
+        TextView directionView = (TextView) findViewById(R.id.stat10);
+        TextView speedView = (TextView) findViewById(R.id.stat12);
+
+        Switch soundSwitch = (Switch) findViewById(R.id.horn);
+        Switch lightSwitch = (Switch) findViewById(R.id.light);
+        Switch smokeSwitch = (Switch) findViewById(R.id.smoke);
+
+        Button forward = (Button) findViewById(R.id.forward);
+        Button backward = (Button) findViewById(R.id.backward);
+
+        SeekBar speedSlider = (SeekBar) findViewById(R.id.speedSlider);
+
+
+        if(locomotive.isHeadLight()) {
+            lightView.setText("An");
+            lightSwitch.setChecked(true);
+        } else{
+            lightView.setText("Aus");
+            lightSwitch.setChecked(false);
+        }
+
+        if(locomotive.isHornSound()) {
+            soundView.setText("An");
+            soundSwitch.setChecked(true);
+        } else{
+            soundView.setText("Aus");
+            soundSwitch.setChecked(false);
+        }
+
+        if(locomotive.isCabineLighting()) {
+            smokeView.setText("An");
+            smokeSwitch.setChecked(true);
+        } else{
+            smokeView.setText("Aus");
+            smokeSwitch.setChecked(false);
+        }
+
+        lightView.setText(locomotive.isHeadLight() ? "An" : "Aus");
+        soundView.setText(locomotive.isHornSound() ? "An" : "Aus");
+        smokeView.setText(locomotive.isCabineLighting() ? "An" : "Aus");
+        trainView.setText(locomotive.getName());
+
+        int direction = locomotive.getDirection();
+        if(direction == 0) {
+            directionView.setText("Vorwärts");
+            forward.setTextColor(Color.BLACK);
+            backward.setTextColor(Color.GRAY);
+        } else if (direction == 1) {
+            directionView.setText("Rückwärts");
+            forward.setTextColor(Color.GRAY);
+            backward.setTextColor(Color.BLACK);
+        }
+
+        speedView.setText("" + Math.abs(locomotive.getSpeed()));
     }
 
     @Override
